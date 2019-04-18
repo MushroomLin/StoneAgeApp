79e7cc918dd522b8902250daeb0d490a59ff7535
MushroomLin <oneera.mushroom@gmail.com>
2019-04-18 16:32:16 -0400

    update picture

--- StoneAgeApp/app/src/main/java/com/example/miniresearchdatabase/viewholder/PostViewHolder.java
+++ StoneAgeApp/app/src/main/java/com/example/miniresearchdatabase/viewholder/PostViewHolder.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.miniresearchdatabase.ImageUtils;
 import com.example.miniresearchdatabase.R;
 import com.example.miniresearchdatabase.models.Post;
 
@@ -16,6 +17,7 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
     public TextView numStarsView;
     public TextView bodyView;
     public TextView addressView;
+    public ImageView pictureView;
 
     public PostViewHolder(View itemView) {
         super(itemView);
@@ -27,6 +29,7 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
         numStarsView = itemView.findViewById(R.id.postNumStars);
         bodyView = itemView.findViewById(R.id.postBody);
         addressView = itemView.findViewById(R.id.postAddress);
+        pictureView = itemView.findViewById(R.id.pictureImageView);
     }
 
     public void bindToPost(Post post, View.OnClickListener starClickListener) {
@@ -35,7 +38,8 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
         numStarsView.setText(String.valueOf(post.starCount));
         bodyView.setText(post.body);
         addressView.setText(post.address);
-
+        if (post.picture!=null)
+            pictureView.setImageBitmap(ImageUtils.stringToBitmap(post.picture));
         starView.setOnClickListener(starClickListener);
     }
 }
