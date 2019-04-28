77be3188ecf37d7e2ef4b0ce2977edecf6c8192a
Weixi Li <xabbfuc@163.com>
2019-04-28 19:41:20 -0400

    update

--- StoneAgeApp/app/src/main/java/com/example/miniresearchdatabase/MainActivity.java
+++ StoneAgeApp/app/src/main/java/com/example/miniresearchdatabase/MainActivity.java
@@ -107,8 +107,8 @@ public class  MainActivity extends BaseActivity implements BottomNavigationView.
                 ft.commit();
                 break;
             case R.id.navigation_map:
-//                startActivity(new Intent(this, MapsActivity.class));
-                startActivity(new Intent(this, SettingActivity.class));
+                startActivity(new Intent(this, MapsActivity.class));
+//                startActivity(new Intent(this, SettingActivity.class));
                 break;
             case R.id.navigation_post:
                 startActivity(new Intent(MainActivity.this, NewPostActivity.class));
